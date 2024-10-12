ea007d2bc0e06b030ceb0497c39bf68befd45fd7
Antonio Miras <antonio@amiras.net>
2024-10-12 21:52:17 +0200

    Billing. Work in progress

---
+++ EasyBillingReports2.Web/Components/Pages/Billing.razor.cs
@@ -0,0 +1,14 @@
+namespace EasyBillingReports2.Web.Components.Pages
+{
+    public partial class Billing
+    {
+        private int _eurHour = 30;
+        private int _hours = 30;
+        private int _total = 457;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
