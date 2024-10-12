3d1ad1bfc0031b25dd35bdebecc1465369408f6e
Antonio Miras <antonio@amiras.net>
2024-10-12 16:21:13 +0200

    Remove unused events

--- EasyBillingReports2.Web/Components/Pages/WorkPeriods.razor.cs
+++ EasyBillingReports2.Web/Components/Pages/WorkPeriods.razor.cs
@@ -16,52 +16,8 @@ namespace EasyBillingReports2.Web.Components.Pages
             _workPeriods = Wpl.WorkPeriods;
         }
 
-        private void OnSlotRender(SchedulerSlotRenderEventArgs args)
+        private void OnAppointmentSelect(SchedulerAppointmentSelectEventArgs<WorkPeriod> args)
         {
-            // Highlight today in month view
-            if (args.View.Text == "Month" && args.Start.Date == DateTime.Today)
-            {
-                args.Attributes["style"] = "background: var(--rz-scheduler-highlight-background-color, rgba(255,220,40,.2));";
-            }
-
-            // Highlight working hours (9-18)
-            if ((args.View.Text == "Week" || args.View.Text == "Day") && args.Start.Hour > 8 && args.Start.Hour < 19)
-            {
-                args.Attributes["style"] = "background: var(--rz-scheduler-highlight-background-color, rgba(255,220,40,.2));";
-            }
-        }
-
-        private async Task OnSlotSelect(SchedulerSlotSelectEventArgs args)
-        {
-        }
-
-        private async Task OnAppointmentSelect(SchedulerAppointmentSelectEventArgs<WorkPeriod> args)
-        {
-            Console.WriteLine("JAremo2");
-        }
-
-        private void OnAppointmentRender(SchedulerAppointmentRenderEventArgs<WorkPeriod> args)
-        {
-            // Never call StateHasChanged in AppointmentRender - would lead to infinite loop
-
-            if (args.Data.Text == "Birthday")
-            {
-                args.Attributes["style"] = "background: red";
-            }
-        }
-
-        private async Task OnAppointmentMove(SchedulerAppointmentMoveEventArgs args)
-        {
-            var draggedAppointment = _workPeriods.FirstOrDefault(x => x == args.Appointment.Data);
-
-            if (draggedAppointment != null)
-            {
-                draggedAppointment.Start = draggedAppointment.Start + args.TimeSpan;
-
-                draggedAppointment.End = draggedAppointment.End + args.TimeSpan;
-
-                await _scheduler.Reload();
-            }
         }
     }
 }
