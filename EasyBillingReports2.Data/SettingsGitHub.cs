0c720b2098f8eb26bd81341a9e1f14a98ad06fe0
Antonio Miras <antonio@amiras.net>
2024-10-13 21:35:19 +0200

    Temporary implementation for SettingsGitHub.cs

---
+++ EasyBillingReports2.Data/SettingsGitHub.cs
@@ -0,0 +1,44 @@
+﻿using EasyBillingReports2.Data.Interfaces;
+using System.Reflection;
+using System.Text.Json;
+
+namespace EasyBillingReports2.Data
+{
+    public class SettingsGitHub : ISettings
+    {
+        private class SettingsGitHubFields
+        {
+            public string Url { get; set; }
+            public string Repo { get; set; }
+            public List<string> Tags { get; set; }
+            public int AmountPerHour { get; set; }
+        }
+
+        private static SettingsGitHubFields _fields = null;
+
+        public SettingsGitHub()
+        {
+            Load();
+        }
+
+        public string Url => _fields.Url;
+        public string Repo => _fields.Repo;
+        public List<string> Tags => _fields.Tags;
+        public int AmountPerHour => _fields.AmountPerHour;
+
+        public void Load()
+        {
+            if (_fields != null)
+            {
+                return;
+            }
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream("EasyBillingReports2.Data.IQVisio.settings.json");
+            var reader = new StreamReader(stream);
+            var json = reader.ReadToEnd();
+
+            _fields = JsonSerializer.Deserialize<SettingsGitHubFields>(json);
+        }
+    }
+}
