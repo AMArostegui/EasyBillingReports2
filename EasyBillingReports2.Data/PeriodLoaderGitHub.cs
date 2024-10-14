c43532214bc050b93a6707fc6f00ff9d150a09bf
Antonio Miras <antonio@amiras.net>
2024-10-14 10:54:45 +0200

    Dummy implementation for PeriodLoaderGitHub

--- EasyBillingReports2.Data/PeriodLoaderGitHub.cs
+++ EasyBillingReports2.Data/PeriodLoaderGitHub.cs
@@ -1,13 +1,31 @@
-﻿
+﻿using EasyBillingReports2.Data.Interfaces;
+using System.Web;
+
 namespace EasyBillingReports2.Data
 {
     public class PeriodLoaderGitHub : IPeriodLoader
     {
-        public List<Period> WorkPeriods => throw new NotImplementedException();
+        private readonly ISettings _settings;
+        private static List<Period> _workPeriods = null;
+
+        public PeriodLoaderGitHub(ISettings settings)
+        {
+            _settings = settings;
+            Load();
+        }
+
+        public List<Period> WorkPeriods => _workPeriods;
 
         public void Load()
         {
-            throw new NotImplementedException();
+            if (_workPeriods != null)
+            {
+                return;
+            }
+
+            var urlDecoded = HttpUtility.UrlDecode(_settings.Url);
+
+            _workPeriods = new List<Period>();
         }
     }
 }
